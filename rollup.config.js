bcc49af5c7590fbfa4ccf15643094016e9074e3f
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-12 19:08:38 +0100

    Prettify

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import postcss from "rollup-plugin-postcss";
-import babel from "rollup-plugin-babel";
+import postcss from 'rollup-plugin-postcss'
+import babel from 'rollup-plugin-babel'
 
 export default {
-  input: "./src/index.js",
+  input: './src/index.js',
   output: {
-    file: "./dist/output.js",
-    format: "esm",
+    file: './dist/output.js',
+    format: 'esm'
   },
   plugins: [
     postcss({
       config: {
-        path: "./postcss.config.js",
+        path: './postcss.config.js'
       },
-      extensions: [".css"],
+      extensions: ['.css'],
       minimize: true,
       inject: {
-        insertAt: "top",
-      },
+        insertAt: 'top'
+      }
     }),
     babel({
-      exclude: "node_modules/**",
-      presets: ["@babel/preset-env", "@babel/preset-react"],
-    }),
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react']
+    })
   ],
-  external: ["react", "react-dom", "framer-motion", "tailwindcss"],
-};
+  external: ['react', 'react-dom', 'framer-motion']
+}
